da84cebd8ed9ccf7eee2ed1e20bed139de80136b
Rahul Bagchi <rahulstech18@gmail.com>
2026-01-20 08:47:13 +0530

    finished showing first 10 images in grid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    // coil compose
+    implementation(libs.coil.compose)
     // compose testing
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/rahulstech/android/infiniteimages/MainActivity.kt
+++ app/src/main/java/rahulstech/android/infiniteimages/MainActivity.kt
@@ -5,28 +5,75 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.toDrawable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import coil.request.ImageRequest
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import rahulstech.android.data.unplash.UnsplashClient
 import rahulstech.android.data.unplash.model.PhotoDto
 import rahulstech.android.infiniteimages.ui.theme.InfiniteImagesTheme
+import androidx.core.graphics.toColorInt
+import coil.compose.AsyncImage
 
 private const val TAG = "MainActivity"
 
+class MainViewModel: ViewModel() {
+
+    private val client = UnsplashClient().api
+
+    private val _photos = MutableStateFlow<List<PhotoDto>>(emptyList())
+
+    val photos: StateFlow<List<PhotoDto>> = _photos.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val res = client.getPhotos()
+                if (res.isSuccessful) {
+                    _photos.value = res.body() ?: emptyList()
+                }
+                else {
+                    Log.e(TAG,"network error; ${res.errorBody()?.string() ?: ""}")
+                }
+            }
+            catch (cause: Throwable) {
+                Log.e(TAG, "initial page loading error", cause)
+            }
+        }
+    }
+}
+
 class MainActivity : ComponentActivity() {
 
-    val client = UnsplashClient()
+    val viewmodel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +82,9 @@ class MainActivity : ComponentActivity() {
             InfiniteImagesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
-                        PhotosGrid(client)
+                        Box(modifier = Modifier.widthIn(max = 800.dp)) {
+                            PhotosGrid(viewmodel)
+                        }
                     }
                 }
             }
@@ -44,26 +93,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotosGrid(client: UnsplashClient) {
-    var photos by remember { mutableStateOf(emptyList<PhotoDto>()) }
-    LaunchedEffect(client) {
-        val res = withContext(Dispatchers.IO) {
-            client.api.getPhotos()
-        }
+fun PhotosGrid(viewmodel: MainViewModel) {
+    val photos by viewmodel.photos.collectAsStateWithLifecycle()
 
-        if (res.isSuccessful) {
-            photos = res.body() ?: emptyList()
-            Log.d(TAG, "photos: $photos")
-        }
-        else {
-            photos = emptyList()
-            Log.e(TAG, res.errorBody()?.string() ?: "")
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(240.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+        items(items = photos, key = { it.id }) { photo ->
+            PhotoGridItem(photo)
         }
     }
+}
 
-//    LazyColumn(modifier = Modifier.fillMaxSize()) {
-//        items(items = photos, key = { it.id }) { photo ->
-//            Text(photo.id, modifier = Modifier.height(36.dp).padding(16.dp))
-//        }
-//    }
+@Composable
+fun PhotoGridItem(photo: PhotoDto) {
+    val context = LocalContext.current
+    val request = remember(photo.urls.thumb) {
+        ImageRequest.Builder(context)
+            .data(photo.urls.thumb)
+            .placeholder(Color(photo.color.toColorInt()).toArgb().toDrawable())
+            .build()
+    }
+
+    AsyncImage(
+        model = request,
+        contentDescription = null,
+        contentScale = ContentScale.FillBounds,
+        modifier = Modifier.aspectRatio(1f).clip(MaterialTheme.shapes.medium),
+        clipToBounds = true,
+    )
 }
