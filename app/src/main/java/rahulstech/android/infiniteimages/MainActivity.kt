30fc2eb61f3f10a1c858a43f1b5696f11a5b9e6b
Rahul Bagchi <rahulstech18@gmail.com>
2026-01-28 22:57:44 +0530

    add refresh loadState UI

--- app/src/main/java/rahulstech/android/infiniteimages/MainActivity.kt
+++ app/src/main/java/rahulstech/android/infiniteimages/MainActivity.kt
@@ -9,15 +9,24 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -28,13 +37,16 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toDrawable
 import androidx.lifecycle.viewModelScope
 import coil.request.ImageRequest
 import rahulstech.android.infiniteimages.ui.theme.InfiniteImagesTheme
 import androidx.core.graphics.toColorInt
 import androidx.lifecycle.AndroidViewModel
+import androidx.paging.LoadState
 import androidx.paging.cachedIn
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
@@ -71,7 +83,7 @@ class MainActivity : ComponentActivity() {
                         contentAlignment = Alignment.TopCenter,
                     ) {
                         Box(modifier = Modifier.widthIn(max = 800.dp)) {
-                            PhotosGrid(viewmodel)
+                            PhotosScreen(viewmodel)
                         }
                     }
                 }
@@ -81,9 +93,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PhotosGrid(viewModel: MainViewModel) {
+fun PhotosScreen(viewModel: MainViewModel) {
     val photos = viewModel.photos.collectAsLazyPagingItems()
 
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        when (photos.loadState.refresh) {
+            is LoadState.Loading -> {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    CircularProgressIndicator()
+
+                    Spacer(modifier = Modifier.width(12.dp))
+
+                    Text("Loading initial images", style = MaterialTheme.typography.bodyLarge)
+                }
+            }
+            is LoadState.Error -> PhotosGridError(onRetry = { photos.retry() })
+            is LoadState.NotLoading -> PhotosGrid(photos)
+        }
+    }
+}
+
+@Composable
+fun PhotosGrid(photos: LazyPagingItems<Photo>) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(240.dp),
         horizontalArrangement = Arrangement.spacedBy(12.dp),
@@ -97,35 +128,21 @@ fun PhotosGrid(viewModel: MainViewModel) {
         }
 
         // Footer load state (append)
-//        when (val append = photos.loadState.append) {
-//            is LoadState.Loading -> {
-//                item(span = { GridItemSpan(maxLineSpan) }) {
-//                    LoadingFooter()
-//                }
-//            }
-//            is LoadState.Error -> {
-//                item(span = { GridItemSpan(maxLineSpan) }) {
-//                    ErrorFooter(
-//                        error = append.error,
-//                        onRetry = { photos.retry() }
-//                    )
-//                }
-//            }
-//            else -> Unit
-//        }
+        when (photos.loadState.append) {
+            is LoadState.Loading -> {
+                items(count = 2) {
+                    PhotoGridItemShimmer()
+                }
+            }
+            is LoadState.Error -> {
+                item {
+                    PhotoGridItemError(onRetry = { photos.retry() })
+                }
+            }
+            else -> Unit
+        }
     }
-
-    // Full-screen refresh state (overlay, not structural)
-//    when (val refresh = photos.loadState.refresh) {
-//        is LoadState.Loading -> FullScreenLoader()
-//        is LoadState.Error -> FullScreenError(
-//            error = refresh.error,
-//            onRetry = { photos.retry() }
-//        )
-//        else -> Unit
-//    }
 }
-
 
 @Composable
 fun PhotoGridItemShimmer() {
@@ -151,5 +168,31 @@ fun PhotoGridItem(photo: Photo) {
         contentScale = ContentScale.FillBounds,
         modifier = Modifier.aspectRatio(1f).clip(MaterialTheme.shapes.medium),
         clipToBounds = true,
+
     )
+}
+
+@Composable
+fun PhotoGridItemError(onRetry: ()-> Unit) {
+    Box(
+        modifier = Modifier.size(240.dp)
+            .clip(MaterialTheme.shapes.medium),
+        contentAlignment = Alignment.Center,
+    ) {
+        TextButton(onClick = onRetry) {
+            Text("Retry", fontSize = 24.sp)
+        }
+    }
+}
+
+@Composable
+fun PhotosGridError(onRetry: ()-> Unit) {
+    Box(
+        modifier = Modifier.fillMaxWidth().height(360.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        TextButton(onClick = onRetry, modifier = Modifier.width(96.dp).height(48.dp)) {
+            Text("Retry", fontSize = 24.sp)
+        }
+    }
 }
